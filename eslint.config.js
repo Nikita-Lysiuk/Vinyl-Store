72536e2ac282c52289be63e66a395e202a097ff5
Nikita.Lysiuk <nikita.lysiuk@skni.umcs.pl>
2024-10-04 16:09:27 +0200

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    languageOptions: {
+        ecmaVersion: 2021,
+        globals: {
+            
+        },
+    },
+    rules: {
+        'indent': ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+    },
+};
