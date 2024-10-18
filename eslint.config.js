bec29979ffd61e59665a14e47219f2765ca5e42d
Nikita Lysiuk <nikita.lisyuk06@gmail.com>
2024-10-18 08:15:41 +0000

    Homework 1

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    languageOptions: {
+        ecmaVersion: 2021,
+        globals: {},
+    },
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+    },
+};
